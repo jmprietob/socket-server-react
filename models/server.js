96ad4dd022bc382c38ce0d136d7d61947cd935c2
José M Prieto <jmprietob@gmail.com>
2020-11-16 18:41:21 +0100

    port

--- models/server.js
+++ models/server.js
@@ -9,7 +9,7 @@ class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.port;
+        this.port = process.env.PORT;
 
         //http server
         this.server = http.createServer(this.app);
